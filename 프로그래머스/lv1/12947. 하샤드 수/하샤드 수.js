d7fedb5dc2b1524a0d6962395840e87b5b36b3d6
jiwanchoi <54405518+jiwanchoi@users.noreply.github.com>
2023-07-16 16:40:48 +0900

    [level 1] Title: 하샤드 수, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12947. 하샤드 수/하샤드 수.js
@@ -0,0 +1,8 @@
+function solution(x) {
+    let arr = String(x).split('');
+    let sum = 0
+    for(let i = 0; i < arr.length; i++) {
+        sum += parseInt(arr[i]);
+    }
+    return (x % sum === 0 ? true : false);
+}
