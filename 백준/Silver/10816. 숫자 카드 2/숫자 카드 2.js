fed4d3ee3fe805329918df7a2887295dcd1b210a
jiwanchoi <54405518+jiwanchoi@users.noreply.github.com>
2023-08-14 23:07:26 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 792 ms, Memory: 161220 KB -BaekjoonHub

---
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.js
@@ -0,0 +1,14 @@
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let N = input[1].split(" ").map(Number);
+let M = input[3].split(" ").map(Number);
+let map = new Map();
+N.forEach((el) => {
+  if (map.has(el)) map.set(el, map.get(el) + 1);
+  else map.set(el, 1);
+});
+let answer = [];
+M.forEach((el) => answer.push(map.get(el) || 0));
+console.log(answer.join(" "));
