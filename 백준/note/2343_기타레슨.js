5819bc0f1df44ab5916922ea03b25fa9ba248d6a
jiwanchoi <54405518+jiwanchoi@users.noreply.github.com>
2023-08-21 17:43:30 +0900

    2343_기타레슨

---
+++ 백준/note/2343_기타레슨.js
@@ -0,0 +1,34 @@
+// let input = require('fs').readFileSync('input.txt').toString().trim().split('\n');
+const fs = require("fs");
+const filePath = process.platform === "linux" ? "/dev/stdin" : "./input.txt";
+let input = fs.readFileSync(filePath).toString().trim().split("\n");
+
+let n = Number(input[0].split(" ")[0]);
+let m = Number(input[0].split(" ")[1]);
+let arr = input[1].split(" ").map(Number);
+
+let left = 1;
+b.forEach((v) => {
+  let start = 0;
+  let end = a.length - 1;
+  let flag = false;
+  while (start <= end) {
+    let mid = parseInt((start + end) / 2);
+
+    if (v < a[mid]) {
+      end = mid - 1;
+    } else if (v > a[mid]) {
+      start = mid + 1;
+    } else {
+      flag = true;
+      break;
+    }
+  }
+  if (flag == true) {
+    answer.push(1);
+  } else {
+    answer.push(0);
+  }
+});
+// console.log(answer.join("\n"));
+console.log(answer);
