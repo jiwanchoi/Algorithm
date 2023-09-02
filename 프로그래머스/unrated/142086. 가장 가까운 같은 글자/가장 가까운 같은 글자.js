de1537f629ce8db2060732e00600391669354eac
jiwanchoi <54405518+jiwanchoi@users.noreply.github.com>
2023-09-02 23:57:21 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 12.40 ms, Memory: 38.9 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -0,0 +1,16 @@
+function solution(s) {
+    let answer = [];
+    let tmp = [];
+    
+    for(let i = 0; i < s.length; i++){
+        if(!tmp.includes(s[i])){
+            answer.push(-1);
+            tmp.push(s[i]);
+        } else if(tmp.includes(s[i])){
+            answer.push(i - tmp.lastIndexOf(s[i]));
+            tmp.push(s[i]);
+        }
+        
+    }
+    return answer;
+}
