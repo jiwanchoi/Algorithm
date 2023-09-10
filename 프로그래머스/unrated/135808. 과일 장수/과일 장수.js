2949bd9799c0bca52d0a2086f2df9baf09d0870c
jiwanchoi <54405518+jiwanchoi@users.noreply.github.com>
2023-09-10 16:09:23 +0900

    [unrated] Title: 과일 장수, Time: 0.07 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/135808. 과일 장수/과일 장수.js
@@ -0,0 +1,10 @@
+function solution(k, m, score) {
+    let answer = 0;
+    score.sort((a, b) => b-a);
+    
+    let count = Math.floor(score.length / m);
+    for(let i=1; i<=count; i++) {
+        answer += score[m*i-1] * m;
+    }
+    return answer;
+}
