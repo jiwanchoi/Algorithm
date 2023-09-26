5c307043a222517aae27072b94e1c728190a596e
jiwanchoi <54405518+jiwanchoi@users.noreply.github.com>
2023-09-26 20:57:35 +0900

    [level 1] Title: 체육복, Time: 0.09 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42862. 체육복/체육복.js
@@ -0,0 +1,14 @@
+function solution(n, lost, reserve) {
+  let lostStudent = lost.filter(v => !reserve.includes(v)).sort();
+  let reserveStudent = reserve.filter(v => !lost.includes(v)).sort();
+
+  for (let i = 0; i < reserveStudent.length; i++) {
+    if (lostStudent.includes(reserveStudent[i] - 1)) {
+      lostStudent = lostStudent.filter(v => v !== reserveStudent[i] - 1);
+    } else if (lostStudent.includes(reserveStudent[i] + 1)) {
+      lostStudent = lostStudent.filter(v => v !== reserveStudent[i] + 1);
+    }
+  }
+
+  return n - lostStudent.length;
+}
